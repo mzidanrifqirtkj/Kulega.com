ca87b9ba546d792082af99c6b76f7746931f5928
mzidanrifqirtkj <mzidanrifqirtkj@gmail.com>
2024-05-28 16:29:02 +0700

    regis

---
+++ resources/views/layouts/metatag.blade.php
@@ -0,0 +1,107 @@
+<!-- Google Tag Manager -->
+<script>
+    (function(w, d, s, l, i) {
+        w[l] = w[l] || [];
+        w[l].push({
+            'gtm.start': new Date().getTime()
+            , event: 'gtm.js'
+        });
+        var f = d.getElementsByTagName(s)[0]
+            , j = d.createElement(s)
+            , dl = l != 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src =
+            'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+    })(window, document, 'script', 'dataLayer', 'GTM-WP4JMQ8Z');
+
+</script>
+<!-- End Google Tag Manager -->
+
+{{-- Meta tags landing page --}}
+<!-- Primary Meta Tags -->
+<title>Layanan konseling dengan pakar profesional dan terpercaya</title>
+<meta name="title" content="Layanan konseling dengan pakar profesional dan terpercaya" />
+<meta name="description" content="Kulega menyediakan layanan konseling secara tatap muka dengan perawatan yang profesional untuk membantu anda mengatasi hambatan yang anda hadapi." />
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://kulega.com/" />
+<meta property="og:title" content="Layanan konseling dengan pakar profesional dan terpercaya" />
+<meta property="og:description" content="Kulega menyediakan layanan konseling secara tatap muka dengan perawatan yang profesional untuk membantu anda mengatasi hambatan yang anda hadapi." />
+<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://kulega.com/" />
+<meta property="twitter:title" content="Layanan konseling dengan pakar profesional dan terpercaya" />
+<meta property="twitter:description" content="Kulega menyediakan layanan konseling secara tatap muka dengan perawatan yang profesional untuk membantu anda mengatasi hambatan yang anda hadapi." />
+<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+<!-- Meta Tags Generated with https://metatags.io -->
+
+{{-- Artikel 1 --}}
+<!-- Primary Meta Tags -->
+<title>Menciptakan Tempat Kerja Sehat: Solusi terbaik untuk mencegah stres</title>
+<meta name="title" content="Menciptakan Tempat Kerja Sehat: Solusi terbaik untuk mencegah stres" />
+<meta name="description" content="Pahami lebih lanjut tentang solusi inovatif untuk mengatasi stres dan meningkatkan kesejahteraan mental di lingkungan kerja." />
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://kulega.com/" />
+<meta property="og:title" content="Menciptakan Tempat Kerja Sehat: Solusi terbaik untuk mencegah stres" />
+<meta property="og:description" content="Pahami lebih lanjut tentang solusi inovatif untuk mengatasi stres dan meningkatkan kesejahteraan mental di lingkungan kerja." />
+<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://kulega.com/" />
+<meta property="twitter:title" content="Menciptakan Tempat Kerja Sehat: Solusi terbaik untuk mencegah stres" />
+<meta property="twitter:description" content="Pahami lebih lanjut tentang solusi inovatif untuk mengatasi stres dan meningkatkan kesejahteraan mental di lingkungan kerja." />
+<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+<!-- Meta Tags Generated with https://metatags.io -->
+
+{{-- Artikel 2 --}}
+<!-- Primary Meta Tags -->
+<title>Manfaat Mindfulness: Teknik Kesadaran Penuh untuk Kesejahteraan Mental</title>
+<meta name="title" content="Manfaat Mindfulness: Teknik Kesadaran Penuh untuk Kesejahteraan Mental" />
+<meta name="description" content="Cari tahu lebih lanjut bagaimana mindfulness dapat meningkatkan produktivitas dan kesejahteraan mental di lingkungan kerja" />
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://kulega.com/" />
+<meta property="og:title" content="Manfaat Mindfulness: Teknik Kesadaran Penuh untuk Kesejahteraan Mental" />
+<meta property="og:description" content="Cari tahu lebih lanjut bagaimana mindfulness dapat meningkatkan produktivitas dan kesejahteraan mental di lingkungan kerja" />
+<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://kulega.com/" />
+<meta property="twitter:title" content="Manfaat Mindfulness: Teknik Kesadaran Penuh untuk Kesejahteraan Mental" />
+<meta property="twitter:description" content="Cari tahu lebih lanjut bagaimana mindfulness dapat meningkatkan produktivitas dan kesejahteraan mental di lingkungan kerja" />
+<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+<!-- Meta Tags Generated with https://metatags.io -->
+
+{{-- Artikel 3 --}}
+<!-- Primary Meta Tags -->
+<title>Meningkatkan Kesejahteraan Mental: Manajemen Tugas di Tempat kerja</title>
+<meta name="title" content="Meningkatkan Kesejahteraan Mental: Manajemen Tugas di Tempat kerja" />
+<meta name="description" content="Pelajari lebih lanjut cara menetapkan prioritas dengan baik untuk mengurangi stres dan meningkatkan produktivitas di tempat kerja" />
+
+<!-- Open Graph / Facebook -->
+<meta property="og:type" content="website" />
+<meta property="og:url" content="https://metatags.io/" />
+<meta property="og:title" content="Meningkatkan Kesejahteraan Mental: Manajemen Tugas di Tempat kerja" />
+<meta property="og:description" content="Pelajari lebih lanjut cara menetapkan prioritas dengan baik untuk mengurangi stres dan meningkatkan produktivitas di tempat kerja" />
+<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+
+<!-- Twitter -->
+<meta property="twitter:card" content="summary_large_image" />
+<meta property="twitter:url" content="https://metatags.io/" />
+<meta property="twitter:title" content="Meningkatkan Kesejahteraan Mental: Manajemen Tugas di Tempat kerja" />
+<meta property="twitter:description" content="Pelajari lebih lanjut cara menetapkan prioritas dengan baik untuk mengurangi stres dan meningkatkan produktivitas di tempat kerja" />
+<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+
+<!-- Meta Tags Generated with https://metatags.io -->
