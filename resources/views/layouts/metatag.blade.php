dae9ca5c55cac47b2e291d4bf981f547d1060ce7
mzidanrifqirtkj <mzidanrifqirtkj@gmail.com>
2024-06-03 14:56:03 +0700

    metaTag image

--- resources/views/layouts/metatag.blade.php
+++ resources/views/layouts/metatag.blade.php
@@ -29,14 +29,14 @@
 <meta property="og:url" content="https://kulega.com/" />
 <meta property="og:title" content="Kulega: Layanan kesehatan mental dengan pakar terpercaya" />
 <meta property="og:description" content="Dapatkan informasi lebih lanjut mengenai program kesehatan mental dan manfaatnya untuk karyawan" />
-<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+<meta property="og:image" content="{{url('/public/assets/landing_page.jpg')}}" />
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content="https://kulega.com/" />
 <meta property="twitter:title" content="Kulega: Layanan kesehatan mental dengan pakar terpercaya" />
 <meta property="twitter:description" content="Dapatkan informasi lebih lanjut mengenai program kesehatan mental dan manfaatnya untuk karyawan" />
-<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+<meta property="twitter:image" content="{{url('/public/assets/landing_page.jpg')}}" />
 
 <!-- Meta Tags Generated with https://metatags.io -->
 
@@ -51,14 +51,14 @@
 <meta property="og:url" content="https://kulega.com/" />
 <meta property="og:title" content="Menciptakan Tempat Kerja Sehat: Solusi terbaik untuk mencegah stres" />
 <meta property="og:description" content="Pahami lebih lanjut tentang solusi inovatif untuk mengatasi stres dan meningkatkan kesejahteraan mental di lingkungan kerja." />
-<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+<meta property="og:image" content="{{url('/public/assets/artikel_1.jpg')}}" />
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content="https://kulega.com/" />
 <meta property="twitter:title" content="Menciptakan Tempat Kerja Sehat: Solusi terbaik untuk mencegah stres" />
 <meta property="twitter:description" content="Pahami lebih lanjut tentang solusi inovatif untuk mengatasi stres dan meningkatkan kesejahteraan mental di lingkungan kerja." />
-<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+<meta property="twitter:image" content="{{url('/public/assets/artikel_1.jpg')}}" />
 
 <!-- Meta Tags Generated with https://metatags.io -->
 
@@ -73,14 +73,14 @@
 <meta property="og:url" content="https://kulega.com/" />
 <meta property="og:title" content="Apa Itu Mindfulness? Teknik Kesadaran Penuh untuk Meningkatkan Kesejahteraan Mental" />
 <meta property="og:description" content="Cari tahu lebih lanjut bagaimana pengaruh mindfulness dapat meningkatkan produktivitas dan kesejahteraan mental di lingkungan kerja" />
-<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+<meta property="og:image" content="{{url('/public/assets/artikel_2.jpg')}}" />
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content="https://kulega.com/" />
 <meta property="twitter:title" content="Apa Itu Mindfulness? Teknik Kesadaran Penuh untuk Meningkatkan Kesejahteraan Mental" />
 <meta property="twitter:description" content="Cari tahu lebih lanjut bagaimana pengaruh mindfulness dapat meningkatkan produktivitas dan kesejahteraan mental di lingkungan kerja" />
-<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+<meta property="twitter:image" content="{{url('/public/assets/artikel_2.jpg')}}" />
 
 <!-- Meta Tags Generated with https://metatags.io -->
 
@@ -95,13 +95,13 @@
 <meta property="og:url" content="https://metatags.io/" />
 <meta property="og:title" content="Meningkatkan Kesejahteraan Mental: Manajemen Tugas di Tempat kerja" />
 <meta property="og:description" content="Pelajari lebih lanjut cara menetapkan prioritas dengan baik untuk mengurangi stres dan meningkatkan produktivitas di tempat kerja" />
-<meta property="og:image" content="https://metatags.io/images/meta-tags.png" />
+<meta property="og:image" content="{{url('/public/assets/artikel_3.jpg')}}" />
 
 <!-- Twitter -->
 <meta property="twitter:card" content="summary_large_image" />
 <meta property="twitter:url" content="https://metatags.io/" />
 <meta property="twitter:title" content="Meningkatkan Kesejahteraan Mental: Manajemen Tugas di Tempat kerja" />
 <meta property="twitter:description" content="Pelajari lebih lanjut cara menetapkan prioritas dengan baik untuk mengurangi stres dan meningkatkan produktivitas di tempat kerja" />
-<meta property="twitter:image" content="https://metatags.io/images/meta-tags.png" />
+<meta property="twitter:image" content="{{url('/public/assets/artikel_3.jpg')}}" />
 
 <!-- Meta Tags Generated with https://metatags.io -->
